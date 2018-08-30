5b4ca7ea219b98e3b8c7a0bb5664c23aaed3f72d
Daniel Schoch <d.schoch90@gmail.com>
2018-08-30 23:46:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,21 @@
 class Triangle
-  attr_accessor :side_one, :side_two, :side_three
+  attr_accessor :a, :b, :c
 
-  def initialize(side_one, side_two, side_three)
+  def initialize(a, b, c)
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
   end
 
+  def valid?
+    @side_one < 0 && @side_two < 0 && @side_three < 0 &&
+  end
+
   def kind
-    
-    
+    if condition
+
+    end
+
   end
 
   class TriangleError < StandardError
